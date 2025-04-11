b5780d510cfd7788f1681bb658876430106d27a2
Hamza Abdelhedi <hamza.abdelhedii@gmail.com>
2025-04-11 00:32:08 -0400

    add package initializer for coco_pipe

---
+++ coco_pipe/__init__.py
@@ -0,0 +1,12 @@
+"""
+Package initializer for the coco_pipe package.
+"""
+
+from .ml import (
+    MLPipeline,
+    pipeline_baseline,
+    pipeline_feature_selection,
+    pipeline_HP_search,
+    pipeline_feature_selection_HP_search,
+    pipeline_unsupervised
+)
